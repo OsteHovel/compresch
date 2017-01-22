36ee804920d490d63dc609c2c8249c15ae2be64a
Oste Hovel (Super) <ostehovel@ostsoft.com>
2017-01-22 22:33:12 +0100

    Initial import of project

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# compresch
-Super Metroid compression
+# Compresch
+Super Metroid compression library in Java
+
+Based upon C++ library writen by Dischmeister and MathOnNapkins
+
+Decompression works but compression is broken
---
+++ src/main/java/com/ostsoft/games/sm/compresch/tree/CrunchNode.java
@@ -0,0 +1,14 @@
+package com.ostsoft.games.sm.compresch.tree;
+
+
+import java.util.LinkedList;
+
+public class CrunchNode {
+    public CrunchNode parent;
+    public Crunch data = new Crunch();
+    public LinkedList<CrunchNode> dn = new LinkedList<>();
+
+    public CrunchNode(CrunchNode parent) {
+        this.parent = parent;
+    }
+}
